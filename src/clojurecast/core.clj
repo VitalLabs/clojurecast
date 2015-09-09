2562d49e8ee4de242bd4a7df7179754147def553
Adrian Medina <adrian.medina@mail.yu.edu>
2015-09-09 12:23:01 -0400

    expose Clojure API for Hazelcast instances

--- src/clojurecast/core.clj
+++ src/clojurecast/core.clj
@@ -30,6 +30,12 @@
    {:pre [*instance*]}
    (.getAtomicLong *instance* name)))
 
+(defn ^com.hazelcast.core.IAtomicReference atomic-reference
+  ([] (atomic-reference "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getAtomicReference *instance* name)))
+
 (defn ^com.hazelcast.core.ClientService client-service
   []
   {:pre [*instance*]}
@@ -73,3 +79,106 @@
   ([^String name]
    {:pre [*instance*]}
    (.getJobTracker *instance* name)))
+
+(defn ^com.hazelcast.core.LifecycleService lifecycle-service
+  []
+  {:pre [*instance*]}
+  (.getLifecycleService *instance*))
+
+(defn ^com.hazelcast.core.IList distributed-list
+  ([] (distributed-list "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getList *instance* name)))
+
+(defn ^com.hazelcast.core.Endpoint local-endpoint
+  []
+  {:pre [*instance*]}
+  (.getLocalEndpoint *instance*))
+
+(defn ^com.hazelcast.core.ILock lock
+  ([] (lock "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getLock *instance* name)))
+
+(defn ^com.hazelcast.logging.LoggingService logging-service
+  []
+  {:pre [*instance*]}
+  (.getLoggingService *instance*))
+
+(defn ^com.hazelcast.core.IMap distributed-map
+  ([] (distributed-map "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getMap *instance* name)))
+
+(defn ^com.hazelcast.core.MultiMap multi-map
+  ([] (multi-map "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getMultiMap *instance* name)))
+
+(defn ^String instance-name
+  []
+  {:pre [*instance*]}
+  (.getName *instance*))
+
+(defn ^com.hazelcast.core.PartitionService partition-service
+  []
+  {:pre [*instance*]}
+  (.getPartitionService *instance*))
+
+(defn ^com.hazelcast.core.IQueue queue
+  ([] (queue "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getQueue *instance* name)))
+
+(defn ^com.hazelcast.quorum.QuorumService quorum-service
+  []
+  {:pre [*instance*]}
+  (.getQuorumService *instance*))
+
+(defn ^com.hazelcast.core.ITopic reliable-topic
+  ([] (reliable-topic "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getReliableTopic *instance* name)))
+
+(defn ^com.hazelcast.core.ReplicatedMap replicated-map
+  ([] (replicated-map "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getReplicatedMap *instance* name)))
+
+(defn ^com.hazelcast.ringbuffer.Ringbuffer ring-buffer
+  ([] (ring-buffer "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getRingbuffer *instance* name)))
+
+(defn ^com.hazelcast.core.ISemaphore semaphore
+  ([] (semaphore "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getSemaphore *instance* name)))
+
+(defn ^com.hazelcast.core.ISet distributed-set
+  ([] (distributed-set "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getSet *instance* name)))
+
+(defn ^com.hazelcast.core.ITopic topic
+  ([] (topic "default"))
+  ([^String name]
+   {:pre [*instance*]}
+   (.getTopic *instance* name)))
+
+(defn ^java.util.concurrent.ConcurrentMap user-context
+  []
+  {:pre [*instance*]}
+  (.getUserContext *instance*))
+
+
