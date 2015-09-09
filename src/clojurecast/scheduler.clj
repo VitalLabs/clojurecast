0a319df17f18d72d4c63c5b312b603b58b32b6ca
Adrian Medina <adrian.medina@mail.yu.edu>
2015-09-09 13:17:42 -0400

    define scheduler lifecycle

--- src/clojurecast/scheduler.clj
+++ src/clojurecast/scheduler.clj
@@ -1,19 +1,38 @@
 (ns clojurecast.scheduler
   (:require [clojurecast.core :as cc]
             [com.stuartsierra.component :as com])
-  (:import [java.util.concurrent Executors ScheduledExecutorService]))
+  (:import [java.util.concurrent Executors ScheduledExecutorService]
+           [com.hazelcast.core Cluster MembershipListener]))
 
-(defrecord Scheduler [jobs ^ScheduledExecutorService exec]
+(defn- scheduler-membership-listener
+  []
+  (let [jobs (cc/multi-map "scheduler/jobs")]
+    (reify MembershipListener
+      (memberAdded [_ e]
+        (println e))
+      (memberAttributeChanged [_ e]
+        (println e))
+      (memberRemoved [_ e]
+        (println e)))))
+
+(defrecord Scheduler [^com.hazelcast.core.MultiMap jobs
+                      ^ScheduledExecutorService exec
+                      ^String listener-id]
   com/Lifecycle
   (start [this]
     (if exec
       this
       (let [jobs (cc/multi-map "scheduler/jobs")
-            exec (Executors/newSingleThreadScheduledExecutor)]
-        (assoc this :exec exec))))
+            exec (Executors/newSingleThreadScheduledExecutor)
+            listener (scheduler-membership-listener)]
+        (assoc this
+          :jobs jobs
+          :exec exec
+          :listener-id (.addMembershipListener (cc/cluster) listener)))))
   (stop [this]
     (if exec
       (do
         (.shutdown exec)
-        (assoc this :exec nil))
+        (.removeMembershipListener (cc/cluster) listener-id)
+        (assoc this :jobs nil :exec nil))
       this)))
