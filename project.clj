edd30c26451cd8d625b2ccf3cdad501eeed1edcd
Adrian Medina <adrian.medina@mail.yu.edu>
2015-10-13 11:55:43 -0400

    update deps

--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Proprietary"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.hazelcast/hazelcast-all "3.5.2"]
+                 [org.apache.storm/storm-core "0.9.5"]
+                 [com.hazelcast/hazelcast-all "3.5.3"]
                  [javax.cache/cache-api "1.0.0"]
-                 [com.stuartsierra/component "0.2.3"]
+                 [com.stuartsierra/component "0.3.0"]
                  [clj-time "0.11.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/test.check "0.8.1"]]
+                                  [org.clojure/test.check "0.8.2"]]
                    :source-paths ["dev"]
                    :jvm-opts ^:replace ["-server"]}})
