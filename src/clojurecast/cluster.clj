d4e79e0ad8fabfe1ab6b06612335007cae711056
Adrian Medina <adrian.medina@mail.yu.edu>
2015-11-04 13:00:51 -0500

    Add extra arity to fns

--- src/clojurecast/cluster.clj
+++ src/clojurecast/cluster.clj
@@ -3,23 +3,33 @@
             [com.stuartsierra.component :as com])
   (:import [com.hazelcast.core Cluster]))
 
-(defn current-time-millis ^long [] (.getClusterTime (cc/cluster)))
+(defn current-time-millis
+  (^long [] (.getClusterTime (cc/cluster)))
+  (^long [instance] (.getClusterTime (cc/cluster instance))))
 
 (defn ^com.hazelcast.core.Member local-member
-  []
-  (.getLocalMember (cc/cluster)))
+  ([]
+   (.getLocalMember (cc/cluster)))
+  ([instance]
+   (.getLocalMember (cc/cluster instance))))
 
 (defn ^java.util.Set members
-  []
-  (.getMembers (cc/cluster)))
+  ([]
+   (.getMembers (cc/cluster)))
+  ([instance]
+   (.getMembers (cc/cluster instance))))
 
 (defn ^String local-member-uuid
-  []
-  (.getUuid (local-member)))
+  ([]
+   (.getUuid (local-member)))
+  ([instance]
+   (.getUuid (local-member instance))))
 
 (defn ^com.hazelcast.core.IMap membership-listeners
-  []
-  (cc/distributed-map "cluster/membership-listeners"))
+  ([]
+   (cc/distributed-map "cluster/membership-listeners"))
+  ([instance]
+   (cc/distributed-map instance "cluster/membership-listeners")))
 
 (defn add-membership-listener
   [listener & {:keys [id]}]
@@ -38,5 +48,7 @@
     (.removeMembershipListener (cc/cluster) id)))
 
 (defn is-master?
-  []
-  (identical? (first (members)) (local-member)))
+  ([]
+   (identical? (first (members)) (local-member)))
+  ([instance]
+   (identical? (first (members instance)) (local-member instance))))
--- src/clojurecast/core.clj
+++ src/clojurecast/core.clj
@@ -33,158 +33,214 @@
   ([] (atomic-long "default"))
   ([name]
    {:pre [*instance*]}
-   (.getAtomicLong *instance* (str name))))
+   (.getAtomicLong *instance* (str name)))
+  ([instance name]
+   (.getAtomicLong instance (str name))))
 
 (defn ^com.hazelcast.core.IAtomicReference atomic-reference
   ([] (atomic-reference "default"))
   ([name]
    {:pre [*instance*]}
-   (.getAtomicReference *instance* (str name))))
+   (.getAtomicReference *instance* (str name)))
+  ([instance name]
+   (.getAtomicReference instance (str name))))
 
 (defn ^com.hazelcast.core.ClientService client-service
-  []
-  {:pre [*instance*]}
-  (.getClientService *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getClientService *instance*))
+  ([instance]
+   (.getClientService instance)))
 
 (defn ^com.hazelcast.core.Cluster cluster
-  []
-  {:pre [*instance*]}
-  (.getCluster *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getCluster *instance*))
+  ([instance]
+   (.getCluster instance)))
 
 (defn ^com.hazelcast.config.Config config
-  []
-  {:pre [*instance*]}
-  (.getConfig *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getConfig *instance*))
+  ([instance]
+   (.getConfig instance)))
 
 (defn ^com.hazelcast.core.ICountDownLatch count-down-latch
   ([] (count-down-latch "default"))
   ([name]
    {:pre [*instance*]}
-   (.getCountDownLatch *instance* (str name))))
+   (.getCountDownLatch *instance* (str name)))
+  ([instance name]
+   (.getCountDownLatch instance (str name))))
 
 (defn ^java.util.Collection distributed-objects
-  []
-  {:pre [*instance*]}
-  (.getDistributedObjects *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getDistributedObjects *instance*))
+  ([instance]
+   (.getDistributedObjects instance)))
 
 (defn ^com.hazelcast.core.IExecutorService executor-service
   ([] (executor-service "default"))
   ([name]
    {:pre [*instance*]}
-   (.getExecutorService *instance* (str name))))
+   (.getExecutorService *instance* (str name)))
+  ([instance name]
+   (.getExecutorService instance (str name))))
 
 (defn ^com.hazelcast.core.IdGenerator id-generator
   ([] (id-generator "default"))
   ([name]
    {:pre [*instance*]}
-   (.getIdGenerator *instance* (str name))))
+   (.getIdGenerator *instance* (str name)))
+  ([instance name]
+   (.getIdGenerator instance (str name))))
 
 (defn ^com.hazelcast.mapreduce.JobTracker job-tracker
   ([] (job-tracker "default"))
   ([name]
    {:pre [*instance*]}
-   (.getJobTracker *instance* (str name))))
+   (.getJobTracker *instance* (str name)))
+  ([instance name]
+   (.getJobTracker instance (str name))))
 
 (defn ^com.hazelcast.core.LifecycleService lifecycle-service
-  []
-  {:pre [*instance*]}
-  (.getLifecycleService *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getLifecycleService *instance*))
+  ([instance]
+   (.getLifecycleService instance)))
 
 (defn ^com.hazelcast.core.IList distributed-list
   ([] (distributed-list "default"))
   ([name]
    {:pre [*instance*]}
-   (.getList *instance* (str name))))
+   (.getList *instance* (str name)))
+  ([instance name]
+   (.getList instance (str name))))
 
 (defn ^com.hazelcast.core.Endpoint local-endpoint
-  []
-  {:pre [*instance*]}
-  (.getLocalEndpoint *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getLocalEndpoint *instance*))
+  ([instance]
+   (.getLocalEndpoint instance)))
 
 (defn ^com.hazelcast.core.ILock lock
   ([] (lock "default"))
   ([name]
    {:pre [*instance*]}
-   (.getLock *instance* (str name))))
+   (.getLock *instance* (str name)))
+  ([instance name]
+   (.getLock instance (str name))))
 
 (defn ^com.hazelcast.logging.LoggingService logging-service
-  []
-  {:pre [*instance*]}
-  (.getLoggingService *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getLoggingService *instance*))
+  ([instance]
+   (.getLoggingService instance)))
 
 (defn ^com.hazelcast.core.IMap distributed-map
   ([] (distributed-map "default"))
   ([name]
    {:pre [*instance*]}
-   (.getMap *instance* (str name))))
+   (.getMap *instance* (str name)))
+  ([instance name]
+   (.getMap instance (str name))))
 
 (defn ^com.hazelcast.core.MultiMap multi-map
   ([] (multi-map "default"))
   ([name]
    {:pre [*instance*]}
-   (.getMultiMap *instance* (str name))))
+   (.getMultiMap *instance* (str name)))
+  ([instance name]
+   (.getMultiMap instance (str name))))
 
 (defn ^String instance-name
-  []
-  {:pre [*instance*]}
-  (.getName *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getName *instance*))
+  ([instance]
+   (.getName instance)))
 
 (defn ^com.hazelcast.core.PartitionService partition-service
-  []
-  {:pre [*instance*]}
-  (.getPartitionService *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getPartitionService *instance*))
+  ([instance]
+   (.getPartitionService instance)))
 
 (defn ^com.hazelcast.core.IQueue queue
   ([] (queue "default"))
   ([name]
    {:pre [*instance*]}
-   (.getQueue *instance* (str name))))
+   (.getQueue *instance* (str name)))
+  ([instance name]
+   (.getQueue instance (str name))))
 
 (defn ^com.hazelcast.quorum.QuorumService quorum-service
-  []
-  {:pre [*instance*]}
-  (.getQuorumService *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getQuorumService *instance*))
+  ([instance]
+   (.getQuorumService instance)))
 
 (defn ^com.hazelcast.core.ITopic reliable-topic
   ([] (reliable-topic "default"))
   ([name]
    {:pre [*instance*]}
-   (.getReliableTopic *instance* (str name))))
+   (.getReliableTopic *instance* (str name)))
+  ([instance name]
+   (.getReliableTopic instance (str name))))
 
 (defn ^com.hazelcast.core.ReplicatedMap replicated-map
   ([] (replicated-map "default"))
   ([name]
    {:pre [*instance*]}
-   (.getReplicatedMap *instance* (str name))))
+   (.getReplicatedMap *instance* (str name)))
+  ([instance name]
+   (.getReplicatedMap instance (str name))))
 
 (defn ^com.hazelcast.ringbuffer.Ringbuffer ring-buffer
   ([] (ring-buffer "default"))
   ([name]
    {:pre [*instance*]}
-   (.getRingbuffer *instance* (str name))))
+   (.getRingbuffer *instance* (str name)))
+  ([instance name]
+   (.getRingbuffer instance (str name))))
 
 (defn ^com.hazelcast.core.ISemaphore semaphore
   ([] (semaphore "default"))
   ([name]
    {:pre [*instance*]}
-   (.getSemaphore *instance* (str name))))
+   (.getSemaphore *instance* (str name)))
+  ([instance name]
+   (.getSemaphore instance (str name))))
 
 (defn ^com.hazelcast.core.ISet distributed-set
   ([] (distributed-set "default"))
   ([name]
    {:pre [*instance*]}
-   (.getSet *instance* (str name))))
+   (.getSet *instance* (str name)))
+  ([instance name]
+   (.getSet instance (str name))))
 
 (defn ^com.hazelcast.core.ITopic topic
   ([] (topic "default"))
   ([name]
    {:pre [*instance*]}
-   (.getTopic *instance* (str name))))
+   (.getTopic *instance* (str name)))
+  ([instance name]
+   (.getTopic instance (str name))))
 
 (defn ^java.util.concurrent.ConcurrentMap user-context
-  []
-  {:pre [*instance*]}
-  (.getUserContext *instance*))
+  ([]
+   {:pre [*instance*]}
+   (.getUserContext *instance*))
+  ([instance]
+   (.getUserContext instance)))
 
 (defn clean-down
   "Destroys all distributed objects managed by Hazelcast. Use with caution."
