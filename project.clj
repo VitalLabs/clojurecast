0a23287140ed132dccb8bdb025d2de9fb24deef6
Adrian Medina <adrian.medina@mail.yu.edu>
2015-11-12 16:05:35 -0500

    add nippy

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [com.hazelcast/hazelcast-all "3.5.2"]
                  [javax.cache/cache-api "1.0.0"]
                  [com.stuartsierra/component "0.2.3"]
+                 [com.taoensso/nippy "2.10.0"]
                  [clj-time "0.11.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.8.1"]]
