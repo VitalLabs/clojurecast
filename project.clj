865d75f9a0e52141e7ce3351d5e7a89b008d95a5
Adrian Medina <adrian.medina@mail.yu.edu>
2015-11-12 14:18:20 -0500

    add async dependency

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license {:name "Proprietary"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.cache "0.6.4"]
                  [com.hazelcast/hazelcast-all "3.5.2"]
                  [javax.cache/cache-api "1.0.0"]
                  [com.stuartsierra/component "0.2.3"]
-                 [org.clojure/core.cache "0.6.4"]
                  [clj-time "0.11.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.8.1"]]
