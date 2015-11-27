0df39057774fa4fae9ea041475e348933cd5b86c
Ian Eslick <eslick@alum.mit.edu>
2015-11-27 13:47:23 -0800

    Fix loss of message listeners after restart
    + Messages listeners only setup once
    + This solution is poor
    
    For now we can't separate reschedule events (add/remove from jobs map)
    from migrations to/from other members so we need to ensure that we have
    a fresh message listener on the local node anytime we add the job back
    to the jobs list.

--- src/clojurecast/scheduler.clj
+++ src/clojurecast/scheduler.clj
@@ -51,12 +51,8 @@
   [job]
   (.set (cc/atomic-reference (:job/id job))
         (assoc job
-          :job/timeout (:job/timeout job 0)
-          :job/state (:job/state job :job.state/running)
-          :job/topic-bus (or (:job/topic-bus job)
-                             (.addMessageListener
-                              (cc/reliable-topic (:job/id job))
-                              (job-message-listener (:job/id job))))))
+               :job/timeout (:job/timeout job 0)
+               :job/state (:job/state job :job.state/running)))
   (.put (cc/multi-map *jobs-name*)
         (cluster/local-member-uuid)
         (:job/id job)))
@@ -87,13 +83,33 @@
     :job/state :job.state/pausing
     :job/timeout 0))
 
+(defn- move-job [jobs job-id old-member-id new-member-id]
+  ;; NOTE: Do this in an HC transaction? This can lose IDs in rare circumstances
+  (.remove jobs old-member-id job-id)
+  (.put jobs new-member-id job-id))
+
 (defn- scheduler-membership-listener
   []
   (let [jobs (cc/multi-map *jobs-name*)]
     (reify MembershipListener
-      (memberAdded [_ e])
+      (memberAdded [_ e]
+        ;; Re-balance tasks when memberAdded
+        (when (cluster/is-master?)
+          (let [new-member (.getMember e)
+                new-id (.getUuid new-member)
+                member-ids (map #(.getUuid ^com.hazelcast.core.Member %)
+                                (cluster/members))
+                all-jobs (.values jobs)
+                target-count (/ (count all-jobs) (count member-ids))]
+            (doseq [member-id member-ids]
+              (let [part (.get jobs member-id)
+                    move-count (- (count part) target-count)]
+                (when (> move-count 0)
+                  (doseq [job-id (take move-count part)]
+                    (move-job jobs job-id member-id new-id))))))))
       (memberAttributeChanged [_ e])
       (memberRemoved [_ e]
+        ;; Distribute tasks when member removed
         (when (cluster/is-master?)
           (let [removed-member (.getMember e)
                 removed-id (.getUuid removed-member)
@@ -104,12 +120,11 @@
                      parts (partition-all (/ (count outstanding) (count members))
                                           outstanding)]
                 (when (seq members)
-                  (let [member (first members)
+                  (let [member-id (first members)
                         job-uuids (first parts)]
                     (doseq [uuid job-uuids]
-                      (.put jobs member uuid))
-                    (recur (next members) (next jobs))))))
-            (.remove jobs removed-id)))))))
+                      (move-job jobs uuid removed-id member-id))
+                    (recur (next members) (next jobs))))))))))))
 
 (defn- ^Callable job-callable
   [^com.hazelcast.core.IAtomicReference job-ref]
@@ -152,16 +167,47 @@
           (= (:job/state job) :job.state/failed) :failed
           :else (run-job job-id exec tasks))))))
 
+(defn remove-job-listener
+  "Remove the current listener from a job, e.g. migrating
+   job to another member"
+  [job-id]
+  (let [job-ref (cc/atomic-reference job-id)
+        job (.get job-ref)]
+    (when-let [listener (:job/topic-bus job)]
+      (.removeMessageListener (cc/reliable-topic job-id)
+                              listener)
+      (.set job-ref (dissoc job :job/topic-bus)))))
+
+(defn add-job-listener [job-id]
+  "Add the job topic listener for the current member"
+  (let [job-ref (cc/atomic-reference job-id)
+        job (.get job-ref)]
+    (.set job-ref
+          (assoc job
+                 :job/topic-bus (.addMessageListener
+                                 (cc/reliable-topic job-id)
+                                 (job-message-listener job-id))))))
+
 (defn- job-entry-listener
   [exec tasks]
   (reify EntryListener
     (entryAdded [_ e]
-      (run-job (.getValue e) exec tasks))
+      (let [job-id (.getValue e)]
+        (add-job-listener job-id)
+        (run-job job-id exec tasks)))
     (entryRemoved [_ e]
       (let [job-id (.getOldValue e)]
+        (remove-job-listener job-id)
         (when-let [^ScheduledFuture task (get tasks job-id)]
           (.cancel task false)
           (swap! tasks dissoc job-id))))))
+
+(defn register-local-jobs
+  "Register every job assigned to this node, should be idempotnet
+   but involves some churn on message listeners and "
+  [jobs]
+  (doseq [job-id (.get jobs (cluster/local-member-uuid))]
+    (reschedule {:job/id job-id})))
 
 (defrecord Scheduler [^com.hazelcast.core.MultiMap jobs
                       ^ScheduledExecutorService exec
@@ -175,20 +221,24 @@
   (-init [this] this)
   (-start [this]
     (let [jobs (cc/multi-map *jobs-name*)
-            exec (doto ^ScheduledThreadPoolExecutor
-                     (Executors/newScheduledThreadPool 1)
-                   (.setRemoveOnCancelPolicy true))
-            tasks (atom {})
-            listener (scheduler-membership-listener)
-            job-listener (job-entry-listener exec tasks)]
-      (assoc this
-          :jobs jobs
-          :exec exec
-          :membership-listener-id (cluster/add-membership-listener listener)
-          :entry-listener-id (.addEntryListener jobs job-listener
-                                                (cluster/local-member-uuid)
-                                                true)
-          :tasks tasks)))
+          exec (doto ^ScheduledThreadPoolExecutor
+                   (Executors/newScheduledThreadPool 1)
+                 (.setRemoveOnCancelPolicy true))
+          tasks (atom {})
+          listener (scheduler-membership-listener)
+          job-listener (job-entry-listener exec tasks)
+          component (assoc this
+                           :jobs jobs
+                           :exec exec
+                           :membership-listener-id (cluster/add-membership-listener listener)
+                           :entry-listener-id (.addEntryListener jobs job-listener
+                                                                 (cluster/local-member-uuid)
+                                                                 true)
+                           :tasks tasks)]
+      ;; Add jobs if master in cluster has already moved them while my
+      ;; listener was not yet scheduled
+      (register-local-jobs jobs)
+      component))
   (-stop [this]
     (.shutdown exec)
     (.removeEntryListener jobs entry-listener-id)
