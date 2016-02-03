b5996e432f6eb9a23d4afa4754fb1eca77bf72df
Adrian Medina <adrian.medina@mail.yu.edu>
2016-02-03 12:18:18 -0500

    Add tests to ensure every job scheduled is available for querying

--- test/clojurecast/scheduler_test.clj
+++ test/clojurecast/scheduler_test.clj
@@ -19,25 +19,39 @@
          :job/state :job.state/paused
          :job/timeout 0))
 
+(def ^:dynamic *jobs* nil)
+
 (defn with-mock-jobs
   "Establishes jobs for each mock system."
   [call-next-method]
-  (let [{:keys [node1 scheduler1 node2 scheduler2]} system]
-    (with-system1
-      (doseq [job (gen/sample job)]
-        (schedule job)))
-    (with-system2
-      (doseq [job (gen/sample job)]
-        (schedule job)))
-    (call-next-method)
-    ;; unschedule everything
-    (with-system1
-      (doseq [[k v] (cluster-jobs)]
-        (unschedule k)))
-    (with-system2
-      (doseq [[k v] (cluster-jobs)]
-        (unschedule k)))))
+  (binding [*jobs* []]
+    (let [{:keys [node1 scheduler1 node2 scheduler2]} system]
+      (with-system1
+        (doseq [job (gen/sample job)]
+          (set! *jobs* (conj *jobs* (:job/id job)))
+          (schedule job)))
+      (with-system2
+        (doseq [job (gen/sample job)]
+          (set! *jobs* (conj *jobs* (:job/id job)))
+          (schedule job)))
+      (call-next-method)
+      ;; unschedule everything
+      (with-system1
+        (doseq [[k v] (cluster-jobs)]
+          (unschedule k)))
+      (with-system2
+        (doseq [[k v] (cluster-jobs)]
+          (unschedule k))))))
 
 (use-fixtures :once with-mock-system with-mock-jobs)
+
+(deftest scheduler-is-bound
+  (is *scheduler* "Scheduler is unbound."))
+
+(deftest every-job-is-available
+  (is *jobs* "Jobs are unbound.")
+  (when *jobs*
+    (doseq [job-id *jobs*]
+      (is (get-job job-id) "Job is not found."))))
 
 
